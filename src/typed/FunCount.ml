d44b480651105de2b942d5f5cbbb053bb9a023d7
Volodya Rachkin <vova@kpnn.ru>
2022-02-17 19:32:13 +0300

    fixed error declarated anonymous function

--- src/typed/FunCount.ml
+++ src/typed/FunCount.ml
@@ -78,6 +78,7 @@ let run {Compile_common.source_file; _} fallback =
         let is_item_has_function (str_item : Typedtree.structure_item) = 
           let is_function (expr : Typedtree.expression) : bool = match expr.exp_type.desc with 
           | Tarrow _ -> true
+          | Tlink _ -> true
           | _ -> false
           in
           let is_binding_has_function (binding : Typedtree.value_binding) : bool = is_function binding.vb_expr in
@@ -89,16 +90,13 @@ let run {Compile_common.source_file; _} fallback =
           in
           List.exists bindings ~f:is_binding_has_function
         in
-        let count_of_fuctions = List.fold_left ~f:(fun acc item -> acc + if is_item_has_function item then 1 else 0) ~init:0 item_list in
-        let filename = List.fold_left ~f:(fun acc item -> 
-          let x = item.str_loc.Location.loc_start.Lexing.pos_fname in
-          if x == acc || acc == "XD ;3" then x else ""
-          ) ~init:"XD ;3" item_list in
+        let count_of_fuctions = List.length @@ List.filter ~f:is_item_has_function item_list in
 
         if count_of_fuctions > 100  then (
         let first_item = List.hd @@ List.filter ~f:is_item_has_function item_list in
         Option.iter first_item ~f:(fun first_item ->
           let loc = first_item.str_loc in
+          let filename = loc.Location.loc_start.Lexing.pos_fname in
           CollectedLints.add ~loc (report ~filename ~loc ("This file contains " ^ string_of_int count_of_fuctions ^ " but less 100 expected!")))
         );
         
--- tests/typed/FunCount.t/run.t
+++ tests/typed/FunCount.t/run.t
@@ -1,7 +1,7 @@
   $ dune build
   $ mylinter -dir .
-  File "FunCount.ml", line 40, characters 0-27:
-  40 | let anime2 x : int =  x + 1
-       ^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  Alert zanuda-linter: This file contains 104 but less 100 expected!
+  File "FunCount.ml", line 39, characters 0-41:
+  39 | let anime : int -> int = (fun x -> x + 1)
+       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  Alert zanuda-linter: This file contains 105 but less 100 expected!
   
